1fc8ac258c8573fc746e578eb65259b187f7298b
Андрій Грицюк <venganpro12@gmail.com>
2023-02-27 18:43:26 +0200

    q

--- Admin.h
+++ Admin.h
@@ -3,12 +3,13 @@
 
 class Admin {
 public:
-	void registerAdmin();
-	void saveLPToFile(const str& login, const str& password) const;
 	bool checkTrueSize(const str& login, int left, int right) const;
 	bool checkFirstSymbol(const str& login) const;
 	bool checkTrueLogin(str& login) const;
 	bool getIsAdmin() const;
+
+	void registerAdmin();
+	void saveLPToFile(const str& login, const str& password) const;
 private:
 	string loginAdm, passwordAdm;
 	bool isAdmin = false;
